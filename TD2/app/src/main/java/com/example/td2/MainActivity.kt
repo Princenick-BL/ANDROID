cb5a944840f2e8e341485d700ddd36f017c003cf
BALLO Prince <pballo@eilcalais.fr>
2021-10-27 11:29:45 +0200

    My first commit

---
+++ TD2/app/src/main/java/com/example/td2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.td2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private Butt
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
