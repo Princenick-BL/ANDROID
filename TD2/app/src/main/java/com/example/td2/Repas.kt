04491bdee4c23714ba0d8e96c26e4177b318b3aa
BALLO Prince <pballo@eilcalais.fr>
2021-10-27 12:05:02 +0200

    updates

--- TD2/app/src/main/java/com/example/td2/Repas.kt
+++ TD2/app/src/main/java/com/example/td2/Repas.kt
@@ -1,11 +1,21 @@
 package com.example.td2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Repas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repas)
+
+        val annuler = findViewById<Button>(R.id.annuler);
+
+        annuler.setOnClickListener {
+            val goToHome =  Intent(this,MainActivity::class.java)
+            startActivity(goToHome)
+            this.finish()
+        }
     }
 }
