5ae094c8878b7e23855327654ea0d38dd780604e
BALLO Prince <pballo@eilcalais.fr>
2021-10-27 12:00:07 +0200

    updates

--- TD2/app/src/main/java/com/example/td2/MainActivity.kt
+++ TD2/app/src/main/java/com/example/td2/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.td2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private Butt
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonRepas = findViewById<Button>(R.id.buttonRepas);
+        val buttonMenu = findViewById<Button>(R.id.buttonMenu);
+
+        buttonRepas.setOnClickListener {
+            val goToRepas =  Intent(this,Repas::class.java)
+            startActivity(goToRepas)
+        }
+        buttonMenu.setOnClickListener {
+            val goToMenu =  Intent(this,Repas::class.java)
+            startActivity(goToMenu)
+        }
     }
 }
