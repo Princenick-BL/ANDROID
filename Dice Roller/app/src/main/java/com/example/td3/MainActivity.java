7a6ffcf323641beab52ae9a263abcfc9e345799d
mariaadissa <80592742+mariaadissa@users.noreply.github.com>
2021-11-16 00:50:33 +0100

    Building project

--- Dice Roller/app/src/main/java/com/example/td3/MainActivity.java
+++ Dice Roller/app/src/main/java/com/example/td3/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.td3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rollButton = (Button) findViewById(R.id.btn);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView resultTextView = (TextView) findViewById(R.id.textView);
+                resultTextView.setText("6");
+
+            }
+        });
     }
+   
 }
