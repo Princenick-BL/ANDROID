f15417908b6c449ad42738bfaa85c3a10d311dd3
Princenick-BL <princenickballo@gmail.com>
2021-12-07 14:14:31 +0100

    CLEAN THE REPO

--- TD2/app/src/main/java/com/example/td2/MainActivity.kt
+++ TD2/app/src/main/java/com/example/td2/MainActivity.kt
@@ -9,14 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonMeteo = findViewById<Button>(R.id.buttonRepas);
-        val buttonMenu = findViewById<Button>(R.id.buttonMenu);
+        val buttonMeteo = findViewById<Button>(R.id.buttonMeteo);
+        val buttonRepas = findViewById<Button>(R.id.buttonRepas);
 
         buttonMeteo.setOnClickListener {
-            startActivity(  Intent(this,Repas::class.java))
+            startActivity(  Intent(this,Meteo_splash::class.java))
         }
-        buttonMenu.setOnClickListener {
-            startActivity(Intent(this,Meteo_splash::class.java))
+        buttonRepas.setOnClickListener {
+            startActivity(Intent(this,Repas::class.java))
         }
 
 
